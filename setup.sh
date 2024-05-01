01385ca09420833e060751cc9944337342dc7568
FangyuanYu <fangyuan.yu@gmail.com>
2024-05-01 23:58:03 +0800

    push

---
+++ setup.sh
@@ -0,0 +1,6 @@
+pip install  --upgrade transformers datasets accelerate evaluate bitsandbytes trl peft torch
+pip install ninja packaging tensorboardX tensorboard
+pip install flash-attn
+pip install --no-deps "xformers<0.0.26" trl peft accelerate bitsandbytes
+pip install git+https://github.com/stanfordnlp/pyreft.git
+pip install dataclasses
